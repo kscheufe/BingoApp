97fb0ca4399c5fa79973ca5210f72d4f507eb0e6
kscheufe <90880062+kscheufe@users.noreply.github.com>
2024-09-04 23:03:26 -0400

    Single db endpoint in server file created and files configured to use it

--- app/server/routes/bingoCards.js
+++ app/server/routes/bingoCards.js
@@ -1,54 +1,53 @@
 const express = require("express");
 const router = express.Router();
-const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./server/db/bingo.db.sqlite");
-
-//get all cards
-router.get('/', (req, res) => {
-    db.all('SELECT * FROM bingo_cards', [], (err, rows) => {
-        if (err) {
-            return res.status(500).json({ error: err.message});
-        }
-        res.json(rows);
+module.exports = function(db) {
+    //get all cards
+    router.get('/', (req, res) => {
+        db.all('SELECT * FROM bingo_cards', [], (err, rows) => {
+            if (err) {
+                return res.status(500).json({ error: err.message});
+            }
+            res.json(rows);
+        });
     });
-});
 
-//add new card (takes 25 numbers (and free), and activity status (on by default))
-router.post('/', (req, res) => {
-    const {card, is_active} = req.body;
-    db.run("INSERT INTO bingo_cards (card, is_active) VALUES (?, ?)", [card, is_active], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message});
-        } 
-        res.json({ id: this.lastID});//return id of the new card
+    //add new card (takes 25 numbers (and free), and activity status (on by default))
+    router.post('/', (req, res) => {
+        const {card, is_active} = req.body;
+        db.run("INSERT INTO bingo_cards (card, is_active) VALUES (?, ?)", [card, is_active], function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message});
+            } 
+            res.json({ id: this.lastID});//return id of the new card
+        });
     });
-});
 
-//edit card? - maybe, seems redundant with the camera/ml. probably handle editing in backend, before api call
+    //edit card? - maybe, seems redundant with the camera/ml. probably handle editing in backend, before api call
 
-//delete single card
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    db.run("DELETE FROM bingo_cards WHERE id = ?", [id], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message});
-        }
-        if (this.changes === 0) {
-            return res.status(404).json({error: "bingo card not found"});
-        }
-        res.json({message: "bingo card deleted successfully"})
+    //delete single card
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        db.run("DELETE FROM bingo_cards WHERE id = ?", [id], function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message});
+            }
+            if (this.changes === 0) {
+                return res.status(404).json({error: "bingo card not found"});
+            }
+            res.json({message: "bingo card deleted successfully"})
+        });
     });
-});
 
-//delete all cards
-router.delete('/', (req, res) => {
-    db.run("DELETE FROM bingo_cards", [], function(err) {
-        if (err) {
-            return res.status(500).json({ error: err.message});
-        }
-        res.json({message: "all bingo cards deleted successfully"})
+    //delete all cards
+    router.delete('/', (req, res) => {
+        db.run("DELETE FROM bingo_cards", [], function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message});
+            }
+            res.json({message: "all bingo cards deleted successfully"})
+        });
     });
-});
 
-module.exports = router;
+    return router;
+}

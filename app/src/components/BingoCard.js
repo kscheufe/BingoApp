ff43f12131d82ffe2f7ba210be932376bc06b9cd
kscheufe <90880062+kscheufe@users.noreply.github.com>
2025-01-01 19:08:49 -0500

    Refactored Bingo Card component to be a view for backend data only
    - Removed forward refs/imperative handles/all data tracking functionality from the component
    - Added new state variables for intended usage
    - Added placeholder methods for new view/controller functionality

--- app/src/components/BingoCard.js
+++ app/src/components/BingoCard.js
@@ -1,4 +1,4 @@
-import React, {forwardRef, useState, useImperativeHandle} from 'react';
+import React, { useState } from 'react';
 import BingoCard from '../objects/BingoCard';
 import "./BingoCard.css";
 
@@ -6,62 +6,37 @@ import "./BingoCard.css";
 //___--------------------------------------------
 
 //special declaration for forward reference from the app.js parent
-const BingoCardComponent = forwardRef((props, ref) => {
+const BingoCardComponent = () => {
     //import winConditionsList as a prop
-    const { winConditionsList } = props;
+    
     const [bingoCard, setBingoCard] = useState(new BingoCard());
+    const [bingoCardList, setBingoCardList] = useState(bingoCard);
+    const [adding, setAdding] = useState(false);
 
-    // Expose handleNumberCall to parent (app.js) via ref
-    useImperativeHandle(ref, () => ({
-        handleNumberCall: handleNumberCall//first is the property passed to parent, second is the method in this script
-    }));
 
-    const handleNumberCall = (calledNumber) => {
-        const num = parseInt(calledNumber);
-        if (isNaN(num)) return;//shouldn't be able to get here due to input restriction, but an extra layer of sanitization
-        bingoCard.markNumber(num);//method in bingoCard object, call originates from app.js and propagates through (App.js -> BingoCard (component) -> BingoCard (class))
-        setBingoCard(bingoCard);//re render
-        //check the win conditions
-        checkWinConditions();
-        //check for wins
-        //winCheck() {}
-        //win conditions Y, T, X, Line, O, arrow, 
-        //just do custom win conditions?
-        //check wins using position-wise AND when comparing the two elements?
-    };
+    const toggleAdding = () =>
+    {
+        console.log(adding? 'cancel' : 'add');
+        setAdding(!adding);
+        //open addCard Area/conditionally render
+    }
 
-    const checkWinConditions = () => {
-        const currentBools = bingoCard.getCardBools();
-        winConditionsList.forEach((winCondition, index) => {
-            let win = true;
-            for (let i = 0; i < winCondition.length; i++) {//for each win conditions
-                for (let j = 0; j <= winCondition[i].length; j++)//for each cell of each win conditions
-                {
-                    if (winCondition[i][j] && !currentBools[i][j])
-                    {
-                        win = false;
-                        break;    
-                    }
-                }
-                if (!win) break;
-            }
-            if (win) {
-                alert(`Win Condition ${index + 1} met!`);
-            }
-            
+    const handleToggleCard = (id) => {
+        console.log('toggle ' + id)
+    }
 
-        })
+    const handleDeleteCard = (id) => {
+        console.log('delete ' + id)
+    }
+
+    const handleInputChange = (rowIndex, colIndex, newValue) => {
+        const updatedNumbers = [...bingoCard.numbers];
+        updatedNumbers[rowIndex][colIndex] = newValue ? parseInt(newValue, 10) : null;
+        setBingoCard({ ...bingoCard, numbers: updatedNumbers });
     }
 
     return (
         <div className="bingoCardContainer">
-            <div>Card Label</div>
-            
-            <div className='bingo-card'>
-                <div className='bingo-header'>
-                    {['B', 'I', 'N', 'G', 'O'].map((letter, index) => (
-                        <div key={`header-${index}`} className='header-cell'>
-                            {letter}    
                         </div>
                     ))}
                 </div>
@@ -76,13 +51,34 @@ const BingoCardComponent = forwardRef((props, ref) => {
                                 >
                                     {num}
                                 </div>
-                            );
-                        })}
-                    </div>
-                ))}
-            </div>
+                            ))}
+                        </div>
+
+                        {/** will need to be replicated for each card in cardsList */}
+
+                        {Array.from({ length: 5 }).map((_, rowIndex) => (
+                            <div key={`row-${rowIndex}`} className="card-row">
+                                {Array.from({ length: 5 }).map((_, colIndex) => {
+                                    const num = bingoCard.getCardNumbers()[colIndex][rowIndex];
+                                    return (
+                                        <div 
+                                            key={`${rowIndex}-${colIndex}`}
+                                            className={`card-cell ${bingoCard.getCardBools()[rowIndex][colIndex] ? 'marked' : ''}`}
+                                        >
+                                            {num}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        )
+                        )}
+                        </div>
+                    </li>
+                    )})
+                ) : ( <p>No Cards Added Yet</p>) }
+            </ul>
         </div>
     )
-});
+};
 
 export default BingoCardComponent;
